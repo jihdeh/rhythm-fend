17754c581243abd5f2722a99c31f9855b987077f
ODEH <luckeyodeh@gmail.com>
2018-04-09 11:23:46 +0100

    Redesigning dashboard

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -1,84 +1,155 @@
 import React, { Component } from "react";
 import "../styles/dashboard.css";
-import WebHeader from "../components/webheader";
-import thumb1 from "../assets/images/thumb1.jpeg";
-
+//import WebHeader from "../components/webheader";
+import nobody from "../assets/images/nobody.jpg";
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+//import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FontIcon from 'material-ui/svg-icons/navigation/menu';
+import IconButton from 'material-ui/IconButton';
 
 export default class DashBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+  handleToggle = () => this.setState({open: !this.state.open});
+
   render() {
     return(
-      <div>
-        <div>
-          <WebHeader />
-        </div>
-        <div className="dashboard-search">
-          <form>
-            <input type="search" name="search" placeholder="Search artiste or song"/>
-            <i class="fas fa-search search-icon"></i>
-          </form>
-          <div className="dashboard-upload">
-            <p>Upload Video</p>
-          </div>
-        </div>
-        <div className="dashboard-body">
-          <div className="row">
-            <div className="col-sm-3">
-              <ul className="dashboard-body__menu1">
-                <li>VIDEOS</li>
-                <li className="menu1-subtext">Liked</li>
-                <li className="menu1-subtext">Loved</li>
-                <li className="menu1-subtext">Commented</li>
-              </ul>
-              <ul class="dashboard-body__menu1">
-                <li>COLLECTIONS</li>
-              </ul>
-            </div>
-            <div className="col-sm-9">
-              <h3>Featured Videos</h3>
-              <div className="row dashboard-body__thumbnails">
-                <div className="col-md-4">
-                  <img src={thumb1} height="206px" width="206px" alt=""/>
-                    <h3>Loren Ipsum</h3>
-                    <p>Loren Ipsum</p>
-                    
-                </div>
-                <div className="col-md-4">
-                  <img src={thumb1} height="206px" width="206px" alt=""/>
-                    <h3>Loren Ipsum</h3>
-                    <p>Loren Ipsum</p>
-                    
-                </div>
-                <div classname="col-md-4">
-                  <img src={thumb1} height="206px" width="206px" alt=""/>
-                    <h3>Loren Ipsum</h3>
-                    <p>Loren Ipsum</p>
-                    
-                </div>
-              </div>
-              <div className="row dashboard-body__thumbnails">
-                <div className="col-md-4">
-                  <img src={thumb1} height="206px" width="206px" alt=""/>
-                    <h3>Loren Ipsum</h3>
-                    <p>Loren Ipsum</p>
-                    
-                </div>
-                <div className="col-md-4">
-                  <img src={thumb1} height="206px" width="206px" alt=""/>
-                    <h3>Loren Ipsum</h3>
-                    <p>Loren Ipsum</p>
-                   
-                </div>
-                <div classname="col-md-4">
-                  <img src={thumb1} height="206px" width="206px" alt=""/>
-                    <h3>Loren Ipsum</h3>
-                    <p>Loren Ipsum</p>
-                    
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <MuiThemeProvider>
+      <div className="content">
+      <div className="mobile-drawer">
+
+          <Drawer open={this.state.open} containerStyle={{marginTop:'8em'}} >
+        <MenuItem>
+          {<div className="dashboard-search">
+              <form className="dashboard-form">
+                <input className="dashboard-input" type="search" name="search" placeholder="Search ..."/>
+                <i class="fas fa-search search-icon"></i>
+              </form>
+            </div>}
+            
+           </MenuItem>
+           
+          <MenuItem>Blog</MenuItem>
+          <MenuItem>Media</MenuItem>
+          <MenuItem>Socials</MenuItem>
+          <MenuItem>Connect</MenuItem>
+          <MenuItem>Music</MenuItem>
+          <MenuItem>Videos</MenuItem>
+          <MenuItem>Most Liked</MenuItem>
+          <MenuItem>Most Viewed</MenuItem>
+          <MenuItem>Winners</MenuItem>
+        </Drawer>    
       </div>
+        <div className="row dashboard-menu">
+          <div className="col-sm-2 dashboard-menu__left">
+            <p>SoundIT Africa</p>
+          </div>
+          <div className="col-sm-10 dashboard-menu__right">
+          <div className="menu-icon">
+          <IconButton tooltip="Font Icon" onClick={this.handleToggle} tooltip="menu">
+          <FontIcon color={'white'} />
+        </IconButton>
+        </div>
+            <div className="right-b">
+              <i class="far fa-envelope right-b__icon"></i>
+              <i class="far fa-bell right-b__icon"></i>
+              <i class="far fa-flag right-b__icon"></i>
+              <div className="right-b__owner">
+                <img className="owner__image" src={nobody} alt=""/>
+                <p>Doe</p>
+              </div>
+              <i class="fas fa-cogs right-b__icon"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className="row dashboard-body">
+          <div className="col-sm-2 dashboard-body__left">
+            <div className="dashboard-search">
+              <form className="dashboard-form">
+                <input className="dashboard-input" type="search" name="search" placeholder="Search ..."/>
+                <i class="fas fa-search search-icon"></i>
+              </form>
+            </div>
+            <div className="dashboard-navigation">
+              <h4>Links</h4>
+              <ul>
+                <li>Blog</li>
+                <li>Media</li>
+                <li>Social</li>
+                <li>Connect</li>
+              </ul>
+              <ul>
+                <li>Music</li>
+                <li>Videos</li>
+                <li>Most Liked</li>
+                <li>Most Viewed</li>
+                <li>Winners</li>
+              </ul>
+            </div>
+          </div>
+          
+          <div className="col-sm-10 dashboard-body">
+            <div className="row head">
+              <div className="col-md-6 col-left">
+                <div className="col1">
+                  <h1>Lorem ipsum</h1>
+                </div>
+
+                <div className="row a">
+                  <div className="col-sm-6 a1">
+                  <h1>Lorem ipsum</h1>
+                  </div>
+                  <div className="col-sm-6 a2">
+                  <h1>Lorem ipsum</h1>
+                  </div>
+                </div>
+
+                <div className="col2">
+                <h1>Lorem ipsum</h1>
+                </div>
+
+                <div className="row b">
+                  <div className="col-sm-6 b1">
+                  <h1>Lorem ipsum</h1>
+                  </div>
+                  <div className="col-sm-6 b2">
+                  <h1>Lorem ipsum</h1>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-right">
+                <div className="col3">
+                <h1>Lorem ipsum</h1>
+                </div>
+
+                <div className="col4">
+                <h1>Lorem ipsum</h1>
+                </div>
+
+                <div className="col5">
+                <h1>Lorem ipsum</h1>
+                </div>
+
+                <div className="row c">
+                  <div className="col-sm-6 c1">
+                  <h1>Lorem ipsum</h1>
+                  </div>
+                  <div className="col-sm-6 c2">
+                  <h1>Lorem ipsum</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </MuiThemeProvider>
     )
   }
 }
