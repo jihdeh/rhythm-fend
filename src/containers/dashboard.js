a981436b6925cc0b2ba0c181975750c4c1bc440c
ODEH <luckeyodeh@gmail.com>
2018-04-11 03:09:40 +0100

    Restyling dashboard

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -1,57 +1,45 @@
 import React, { Component } from "react";
 import "../styles/dashboard.css";
-//import WebHeader from "../components/webheader";
 import nobody from "../assets/images/nobody.jpg";
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-//import RaisedButton from 'material-ui/RaisedButton';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import FontIcon from 'material-ui/svg-icons/navigation/menu';
-import IconButton from 'material-ui/IconButton';
 
 export default class DashBoard extends Component {
+  
   constructor(props) {
     super(props);
-    this.state = {open: false};
+   
   }
-  handleToggle = () => this.setState({open: !this.state.open});
-
-  render() {
+ 
+  render() { 
     return(
-      <MuiThemeProvider>
       <div className="content">
-      <div className="mobile-drawer">
-
-          <Drawer open={this.state.open} containerStyle={{marginTop:'8em'}} >
-        <MenuItem>
-          {<div className="dashboard-search">
-              <form className="dashboard-form">
-                <input className="dashboard-input" type="search" name="search" placeholder="Search ..."/>
-                <i class="fas fa-search search-icon"></i>
-              </form>
-            </div>}
-            
-           </MenuItem>
-          <MenuItem>Blog</MenuItem>
-          <MenuItem>Media</MenuItem>
-          <MenuItem>Socials</MenuItem>
-          <MenuItem>Connect</MenuItem>
-          <MenuItem>Music</MenuItem>
-          <MenuItem>Videos</MenuItem>
-          <MenuItem>Most Liked</MenuItem>
-          <MenuItem>Most Viewed</MenuItem>
-          <MenuItem>Winners</MenuItem>
-        </Drawer>    
-      </div>
+      
         <div className="row dashboard-menu">
           <div className="col-sm-2 dashboard-menu__left">
             <p>SoundIT Africa</p>
+            <div className="mobile-search">
+              <form className="mobile-form">
+                <input className="mobile-input" type="search" name="search" placeholder="Search ..."/>
+                <i class="fas fa-search mobile-search-icon"></i>
+              </form>
+            </div>
           </div>
           <div className="col-sm-10 dashboard-menu__right">
           <div className="menu-icon">
-          <IconButton tooltip="Font Icon" onClick={this.handleToggle} tooltip="menu">
-          <FontIcon color={'white'} />
-        </IconButton>
+            <input type="checkbox" id="drawer-toggle" name="drawer-toggle"/>
+          <label for="drawer-toggle" id="drawer-toggle-label"></label>
+          <nav id="drawer">
+            <ul className="drawer-menu">
+              <li><a href="#">Vote</a></li>
+              <li><a href="#">Comment</a></li>
+              <li><a href="#">Social</a></li>
+              <li><a href="#">Connect</a></li>
+              <li><a href="#">Music</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">Most Liked</a></li>
+              <li><a href="#">Most Viewed</a></li>
+              <li><a href="#">Winners</a></li>
+            </ul>
+        </nav>
         </div>
             <div className="right-b">
               <i class="far fa-envelope right-b__icon"></i>
@@ -60,8 +48,7 @@ export default class DashBoard extends Component {
               <div className="right-b__owner">
                 <img className="owner__image" src={nobody} alt=""/>
                 <p>Doe</p>
-              </div>
-              <i class="fas fa-cogs right-b__icon"></i>
+              </div> 
             </div>
           </div>
         </div>
@@ -75,19 +62,16 @@ export default class DashBoard extends Component {
               </form>
             </div>
             <div className="dashboard-navigation">
-              <h4>Links</h4>
               <ul>
-                <li>Blog</li>
-                <li>Media</li>
-                <li>Social</li>
-                <li>Connect</li>
-              </ul>
-              <ul>
-                <li>Music</li>
-                <li>Videos</li>
-                <li>Most Liked</li>
-                <li>Most Viewed</li>
-                <li>Winners</li>
+                <li><a href="#">Vote</a></li>
+                <li><a href="#">Comment</a></li>
+                <li><a href="#">Social</a></li>
+                <li><a href="#">Connect</a></li>
+                <li><a href="#">Music</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Most Liked</a></li>
+                <li><a href="#">Most Viewed</a></li>
+                <li><a href="#">Winners</a></li>
               </ul>
             </div>
           </div>
@@ -98,57 +82,31 @@ export default class DashBoard extends Component {
                 <div className="col1">
                   <h1>Lorem ipsum</h1>
                 </div>
-
-                <div className="row a">
-                  <div className="col-sm-6 a1">
-                  <h1>Lorem ipsum</h1>
-                  </div>
-                  <div className="col-sm-6 a2">
-                  <h1>Lorem ipsum</h1>
-                  </div>
-                </div>
-
-                <div className="col2">
-                <h1>Lorem ipsum</h1>
-                </div>
-
                 <div className="row b">
                   <div className="col-sm-6 b1">
-                  <h1>Lorem ipsum</h1>
+                    <h1>Lorem ipsum</h1>
                   </div>
                   <div className="col-sm-6 b2">
-                  <h1>Lorem ipsum</h1>
+                    <h1>Lorem ipsum</h1>
                   </div>
                 </div>
               </div>
               <div className="col-md-6 col-right">
                 <div className="col3">
-                <h1>Lorem ipsum</h1>
+                  <h1>Lorem ipsum</h1>
                 </div>
 
                 <div className="col4">
-                <h1>Lorem ipsum</h1>
-                </div>
-
-                <div className="col5">
-                <h1>Lorem ipsum</h1>
-                </div>
-
-                <div className="row c">
-                  <div className="col-sm-6 c1">
                   <h1>Lorem ipsum</h1>
-                  </div>
-                  <div className="col-sm-6 c2">
-                  <h1>Lorem ipsum</h1>
-                  </div>
                 </div>
+                
               </div>
             </div>
           </div>
         </div>
 
       </div>
-      </MuiThemeProvider>
+      
     )
   }
 }
