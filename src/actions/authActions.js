f0da53a8d3f67f9e21c370f0f7e204b584aef507
jihdeh <jide.b.tade@gmail.com>
2018-04-27 00:39:57 +0100

    log process

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -10,6 +10,7 @@ import {
 // import { displayError } from "./errorActions";
 
 export const login = ({ email, password }) => dispatch => {
+  console.log(process.env);
   axios
     .post(`${process.env.REACT_APP_BASE_URL}/auth/signin`, { email, password })
     .then(({ data }) => {
