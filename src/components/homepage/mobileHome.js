4d8e1b3455eb024b296be218109a24281e7a1ca9
Lucky Odeh <luckeyodeh@gmail.com>
2018-05-07 11:10:13 +0100

    more styling

--- src/components/homepage/mobileHome.js
+++ src/components/homepage/mobileHome.js
@@ -1,16 +1,18 @@
 import React from "react";
 import LoginPage from "../login";
+import { Link } from "react-router-dom";
 
 const MobileHome = () => {
   return (
     <div className="mobile-sa-text">
       <span className="mobile-sa-text__title">
-        <img
-          src="https://images.vexels.com/media/users/3/131548/isolated/preview/9e36529b6e31cc4bae564fc2d14a8d0f-msica-icono-de-la-nota-c-rculo-by-vexels.png"
-          className="mobile-logo"
-          style={{ width: "150px" }}
-          alt=""
-        />
+        <Link to="/">
+          <img
+            alt="soundit logo"
+            className="mobile-sa-logo"
+            src="/images/logo.png"
+          />
+        </Link>
         <h1>SounditAfrica!</h1>
         <h4>Be the next Rising Gospel Star</h4>
         <LoginPage />
