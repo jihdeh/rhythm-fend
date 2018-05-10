59c8e901f2b8aee770f96285cebbd766a3bb5a65
Lucky Odeh <luckeyodeh@gmail.com>
2018-05-10 15:15:11 +0100

    removed search input

--- src/components/searchpage/contestantSearchView.js
+++ src/components/searchpage/contestantSearchView.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "../../styles/searchpage.css";
-import SearchInput from "../authHeaders/searchInput"
 
 class ContestantView extends Component {
   render() {
