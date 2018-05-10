9293399c679e138d327d4caf5de9cb4f9bf3504e
Lucky Odeh <luckeyodeh@gmail.com>
2018-05-10 15:21:43 +0100

    removed search input

--- src/components/authHeaders/searchInput.js
+++ src/components/authHeaders/searchInput.js
@@ -4,10 +4,9 @@ class SearchInput extends Component {
   render() {
     const { containerStyle, inputStyle } = this.props;
     return (
-      <form id="search-input-form" className={containerStyle}>
+      <form className={containerStyle}>
         <input
           className={inputStyle}
-          id="contestant-searchinput"
           type="search"
           name="search"
           placeholder="Search for contestant"
