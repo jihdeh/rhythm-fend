0f9aed21cd8902b107a4207871cb4a2d1251e47f
Lucky Odeh <luckeyodeh@gmail.com>
2018-05-12 19:27:39 +0100

    added login form and text to slide

--- client/src/components/homepage/slider.js
+++ client/src/components/homepage/slider.js
@@ -3,21 +3,51 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../styles/slider.css";
+import "../../styles/home.css";
+import LoginPage from "../login";
 
 class Slide extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {displayForm: false};
+  }
   render() {
     const settings = {
       dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000
     };
+    const displayForm = this.state.displayForm;
     return (
       <div className="slider-menu">
         <h2 className="slider-menu__title" />
         <Slider {...settings}>
-          <div className="banner" />
+          <div className="banner">
+            {displayForm ? <div className="row">
+              <div className="sa-text">
+                <span className="sa-text__title">
+                  <h1 className="sa-text-title__company-name">
+                    SoundIT{" "}
+                    <img
+                      className="sa-brand__name-logo"
+                      src="/images/continent-icon-africa.png"
+                      alt=""
+                    />frica.
+                  </h1>
+                  <h4>Be the next Rising Gospel Star</h4>
+                  <LoginPage />
+                </span>
+              </div>
+            </div> : 
+            <div className="sa-text__promo">
+              <h2>Stand a chance to win $5000</h2>
+            </div>}
+          </div>
           <div>
             <h3>Hi</h3>
           </div>
