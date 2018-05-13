6196d34f3ffc18873a3f37655d6c39d71ef40407
johnanisere <johnanisere@gmail.com>
2018-05-13 16:16:40 +0100

    added username verification with activity indicators

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -30,6 +30,7 @@ export const formatString = _ => {
 };
 
 export const firstLetter = _ => _.charAt(0).toUpperCase();
+
 export default {
   mybio: `It is a long established fact that a reader will be distracted by the readable content of
 a page when looking at its layout. The point of using Lorem Ipsum is that it has a
