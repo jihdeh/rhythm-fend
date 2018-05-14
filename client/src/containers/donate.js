ea9428558c924fc1ef8c402d787e7fdcbe83c256
Lucky Odeh <luckeyodeh@gmail.com>
2018-05-14 13:06:03 +0100

    redesigned donate page

--- client/src/containers/donate.js
+++ client/src/containers/donate.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { donate } from "../actions/miscActions";
 import "../styles/donate.css";
+import { Link } from "react-router-dom";
+
 
 class Donate extends Component {
   state = {
@@ -69,7 +71,92 @@ class Donate extends Component {
     const { loadingPaystackModule, successfulDonation, name } = this.state;
     return (
       <div className="donate-container">
-        <form className="donate-container__form">
+                  <div className="wrap">
+              <div className="donate-left">
+               
+              </div>
+              <div className="donate-main donate-subscribe">
+                
+                <form className="donate-container__form">
+          {!successfulDonation ? (
+            <span>
+              <h3 className="donate-container__form-reason">
+                Please for appreciation! we'd like some information about you
+              </h3>
+              <label>
+                Name or Organization name: <small>*</small>
+              </label>
+              <input
+                className="donate-input"
+                type="text"
+                onChange={({ target }) => this.setState({ name: target.value })}
+                placeholder="Your name or organization name e.g One Love International"
+              />
+              <label>Phone number</label>
+              <input
+                className="donate-input"
+                type="number"
+                onChange={({ target }) =>
+                  this.setState({ phoneNumber: target.value })
+                }
+                placeholder="Phonenumber"
+              />
+              <label>Your Email Sir/Ma</label>
+              <input
+                className="donate-input"
+                type="email"
+                onChange={({ target }) =>
+                  this.setState({ email: target.value })
+                }
+              />
+              <label>
+                How much would you like to donate: <small>*</small>
+              </label>
+              <input
+                className="donate-input"
+                type="number"
+                onChange={({ target }) =>
+                  this.setState({ amount: target.value })
+                }
+                placeholder="How much donation?"
+              />
+              {loadingPaystackModule ? (
+                <button className="donate-submit" disabled>
+                  <img src="/images/Spinner-1s-50px.svg" alt="" />{" "}
+                </button>
+              ) : (
+                <input
+                  className="donate-submit"
+                  onClick={this.submit}
+                  value="Donate"
+                  type="submit"
+                />
+              )}
+            </span>
+          ) : (
+            <span className="donation-thanks">
+              <img
+                alt="confetti"
+                src="https://d30y9cdsu7xlg0.cloudfront.net/png/743250-200.png"
+              />
+              <h2>Thank you for your donations {name}! </h2>
+              <p>
+                We sincerely appreciate your kindness towards the success of our
+                show.
+              </p>
+              <p>
+                In the spirit of giving, we would be giving half of your
+                donations to charity.
+              </p>
+            </span>
+          )}
+        </form>
+                  
+                
+              </div>
+              
+            </div>
+        {/*<form className="donate-container__form">
           {!successfulDonation ? (
             <span>
               <h3 className="donate-container__form-reason">
@@ -142,7 +229,7 @@ class Donate extends Component {
               </p>
             </span>
           )}
-        </form>
+        </form>*/}
       </div>
     );
   }
