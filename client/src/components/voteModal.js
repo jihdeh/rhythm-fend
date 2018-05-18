fd17c76b466d62430ca06e1d76e60e61d09f0606
johnanisere <johnanisere@gmail.com>
2018-05-18 23:02:49 +0100

    fixe newVote feature

--- client/src/components/voteModal.js
+++ client/src/components/voteModal.js
@@ -27,96 +27,104 @@ export default class voteModal extends Component {
         show={showcastvote}
       >
         <Modal.Body className="my--modal--cont">
-          <span className="vote--modal">
-            <span className="vote--modal--top--cont">
-              <span className="vote--modal--title">select amount</span>
-              <span className="vote--modal--amount-cont">
-                <span
-                  className={
-                    voteCount === 50
-                      ? "vote--modal--amount vote--select--active"
-                      : "vote--modal--amount"
-                  }
-                  onClick={() => {
-                    onchangeVoteAmount(50);
-                    this.refs.amountInput.value = "";
-                  }}
-                >
-                  50
+          {loadingPaystack ? (
+            <span className="loading--payment">Loading Payment Module...</span>
+          ) : (
+            <span className="vote--modal">
+              <span className="vote--modal--top--cont">
+                <span className="vote--modal--title">
+                  select number of votes
                 </span>
-                <span
-                  className={
-                    voteCount === 75
-                      ? "vote--modal--amount vote--select--active"
-                      : "vote--modal--amount"
-                  }
-                  onClick={() => {
-                    onchangeVoteAmount(75);
-                    this.refs.amountInput.value = "";
-                  }}
-                >
-                  75
+                <span className="vote--modal--amount-cont">
+                  <span
+                    className={
+                      voteCount === 50
+                        ? "vote--modal--amount vote--select--active"
+                        : "vote--modal--amount"
+                    }
+                    onClick={() => {
+                      onchangeVoteAmount(50);
+                      this.refs.amountInput.value = "";
+                    }}
+                  >
+                    50
+                  </span>
+                  <span
+                    className={
+                      voteCount === 75
+                        ? "vote--modal--amount vote--select--active"
+                        : "vote--modal--amount"
+                    }
+                    onClick={() => {
+                      onchangeVoteAmount(75);
+                      this.refs.amountInput.value = "";
+                    }}
+                  >
+                    75
+                  </span>
+                </span>
+                <span className="vote--modal--amount-cont">
+                  <span
+                    className={
+                      voteCount === 100
+                        ? "vote--modal--amount vote--select--active"
+                        : "vote--modal--amount"
+                    }
+                    onClick={() => {
+                      onchangeVoteAmount(100);
+                      this.refs.amountInput.value = "";
+                    }}
+                  >
+                    100
+                  </span>
+                  <span
+                    className={
+                      voteCount === 200
+                        ? "vote--modal--amount vote--select--active"
+                        : "vote--modal--amount"
+                    }
+                    onClick={() => {
+                      onchangeVoteAmount(200);
+                      this.refs.amountInput.value = "";
+                    }}
+                  >
+                    200
+                  </span>
                 </span>
               </span>
-              <span className="vote--modal--amount-cont">
-                <span
-                  className={
-                    voteCount === 100
-                      ? "vote--modal--amount vote--select--active"
-                      : "vote--modal--amount"
-                  }
-                  onClick={() => {
-                    onchangeVoteAmount(100);
-                    this.refs.amountInput.value = "";
-                  }}
-                >
-                  100
+              <span className="vote--modal--bottom--cont">
+                <span className="vote--modal--input--title">
+                  or enter an amount
                 </span>
-                <span
+                <input
+                  ref="amountInput"
                   className={
-                    voteCount === 200
-                      ? "vote--modal--amount vote--select--active"
-                      : "vote--modal--amount"
+                    focus
+                      ? "vote--modal--input vote--select--active"
+                      : "vote--modal--input"
                   }
-                  onClick={() => {
-                    onchangeVoteAmount(200);
-                    this.refs.amountInput.value = "";
-                  }}
-                >
-                  200
-                </span>
+                  type="number"
+                  onFocus={() => this.setState({ focus: true })}
+                  onBlur={() => this.setState({ focus: false })}
+                  onChange={onchangeVoteCount}
+                />
               </span>
+              {!loadingPaystack ? (
+                <label
+                  className="vote--modal--btn"
+                  onClick={
+                    voteCount
+                      ? onVote
+                      : () => alert("Please enter amount of votes")
+                  }
+                >
+                  VOTE
+                </label>
+              ) : (
+                <label className="vote--modal--btn">Please hold</label>
+              )}
             </span>
-            <span className="vote--modal--bottom--cont">
-              <span className="vote--modal--input--title">
-                or enter an amount
-              </span>
-              <input
-                ref="amountInput"
-                className={
-                  focus
-                    ? "vote--modal--input vote--select--active"
-                    : "vote--modal--input"
-                }
-                type="number"
-                onFocus={() => this.setState({ focus: true })}
-                onBlur={() => this.setState({ focus: false })}
-                onChange={onchangeVoteCount}
-              />
-            </span>
-            {!loadingPaystack ? (
-              <label
-                className="vote--modal--btn"
-                onClick={
-                  voteCount ? onVote : alert("Please enter amount of votes")
-                }
-              >
-                VOTE
-              </label>
-            ) : (
-              <label className="vote--modal--btn">Please hold</label>
-            )}
-          </span>
+          )}
         </Modal.Body>
       </Modal>
     );
