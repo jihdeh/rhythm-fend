ec65d7c1323ec3f1d9fd01645acb3cb08506ff35
johnanisere <johnanisere@gmail.com>
2018-05-19 19:14:23 +0100

    added youtube url feature

--- client/src/components/dashboard/contestantView.js
+++ client/src/components/dashboard/contestantView.js
@@ -2,109 +2,157 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import "../../styles/dashboard.css";
+import videoparser from "js-video-url-parser";
+import "js-video-url-parser/lib/provider/youtube";
 
 class ContestantView extends Component {
-  constructor (props) {
-    super (props);
-    this.state = {hasVideo: false};
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasVideo: false,
+      videourl: "",
+      url: ""
+    };
   }
+  onSave = async => {
+    try {
+      const { url } = this.state;
+      let videourl = videoparser.create({
+        videoInfo: videoparser.parse(url),
+        format: "embed"
+      });
+      this.setState({
+        hasVideo: true,
+        videourl
+      });
+    } catch (e) {
+      console.log("error", e.message);
+      alert("Invalid URL");
+    }
+  };
   render() {
     const { user } = this.props;
     const { token } = user;
-    const hasVideo = this.state.hasVideo;
+    const { hasVideo, videourl } = this.state;
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="voter-container">
-              <p className="voter-section__title">
-                <b>HI {token.firstName}</b>
-                <span>good to have you on here!</span>
-              </p>
-            
+            <p className="voter-section__title">
+              <b>HI {token.firstName}</b>
+              <span>good to have you on here!</span>
+            </p>
+
             <h2>Profile Details</h2>
             <div className="col-contestant-fav">
-            <div className="col-sm-6 col-md-6 col-lg-6">
-              <div className="col-contestant-profile-photo">
-                <img
-                  className="dashboard-profile__image img-responsive"
-                  src="../../images/nobody.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="col-contestant-social-icons">
-                <a href="">Public Profile</a>
-                <div className="contestants-media-icons">
-                  <a href=""><i className="social-profile fab fa-facebook-f" /> </a>
-                  <a href=""><i className="social-profile fab fa-twitter" /> </a>
-                  <a href=""><i className="social-profile fab fa-instagram" /></a>
-                </div>
-              </div>
-              <div className="col-contestant-bio">
-                <h2>Jim James</h2>
-                <p>Jim James is a sales marketer from ABC Industries, Lagos, an inbound marketing and sales platform that helps companies attract visitors, convert leads, and close customers. He graduated with honors from Lagos State University with a dual degree in Business Administration and Creative Writing.</p>
-              </div>
-            </div>
-            
-            <div className="col-sm-6 col-md-4 col-lg-4">
-              <div className="col-contestant-profile"> 
-                <div className="col-contestant-profile-items">
-                  <p className="col-contestant-profile__key">First Name:</p>
-                  <p className="col-contestant-profile__value">John</p>
-                </div>
-                <div className="col-contestant-profile-items">
-                  <p className="col-contestant-profile__key">Last Name:</p>
-                  <p className="col-contestant-profile__value">Doe</p>
-                </div>
-                <div className="col-contestant-profile-items">
-                  <p className="col-contestant-profile__key">Username:</p>
-                  <p className="col-contestant-profile__value">johndoe</p>
-                </div>
-                <div className="col-contestant-profile-items">
-                  <p className="col-contestant-profile__key">Email:</p>
-                  <p className="col-contestant-profile__value">johndoe@x.com</p>
-                </div>
-                <div className="col-contestant-profile-items">
-                  <p className="col-contestant-profile__key">Country:</p>
-                  <p className="col-contestant-profile__value">Nigeria</p>
-                </div>
-                <div className="col-contestant-profile-items">
-                  <p className="col-contestant-profile__key">State:</p>
-                  <p className="col-contestant-profile__value">Lagos</p>
-                </div>
-                {hasVideo ? (
-                <div>
-                  <p className="col-contestant-profile__key">YouTube Video:</p>
-                  <iframe
-                    width="100%"
-                    height="200px"
-                    src="https://www.youtube.com/embed/-oCCnxBos10"
-                    title="Contestant video 2"
-                    frameBorder="0"
-                    allow="autoplay; encrypted-media"
-                    allowFullScreen
+              <div className="col-sm-6 col-md-6 col-lg-6">
+                <div className="col-contestant-profile-photo">
+                  <img
+                    className="dashboard-profile__image img-responsive"
+                    src="../../images/nobody.jpg"
+                    alt=""
                   />
                 </div>
-                ) : (
-                <div className="youtube-video-upload">
-                  <div className="youtube-video-container">
-                    <p>Please provide the link to your youtube video</p>
+                <div className="col-contestant-social-icons">
+                  <a href="">Public Profile</a>
+                  <div className="contestants-media-icons">
+                    <a href="">
+                      <i className="social-profile fab fa-facebook-f" />{" "}
+                    </a>
+                    <a href="">
+                      <i className="social-profile fab fa-twitter" />{" "}
+                    </a>
+                    <a href="">
+                      <i className="social-profile fab fa-instagram" />
+                    </a>
                   </div>
-                  <input
-                    className="youtube-video-input"
-                    type="text"
-                    placeholder="Your youtube link here"
-                  />
-                  <span>
-                    <button className="youtube-video-save-button">Save Video</button>
-                  </span>
                 </div>
-                )}
-                <div className="col-contestant-button">
-                  <button className="col-contestant-update-button">Update Profile</button>
+                <div className="col-contestant-bio">
+                  <h2>Jim James</h2>
+                  <p>
+                    Jim James is a sales marketer from ABC Industries, Lagos, an
+                    inbound marketing and sales platform that helps companies
+                    attract visitors, convert leads, and close customers. He
+                    graduated with honors from Lagos State University with a
+                    dual degree in Business Administration and Creative Writing.
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="col-contestant-profile">
+                  <div className="col-contestant-profile-items">
+                    <p className="col-contestant-profile__key">First Name:</p>
+                    <p className="col-contestant-profile__value">John</p>
+                  </div>
+                  <div className="col-contestant-profile-items">
+                    <p className="col-contestant-profile__key">Last Name:</p>
+                    <p className="col-contestant-profile__value">Doe</p>
+                  </div>
+                  <div className="col-contestant-profile-items">
+                    <p className="col-contestant-profile__key">Username:</p>
+                    <p className="col-contestant-profile__value">johndoe</p>
+                  </div>
+                  <div className="col-contestant-profile-items">
+                    <p className="col-contestant-profile__key">Email:</p>
+                    <p className="col-contestant-profile__value">
+                      johndoe@x.com
+                    </p>
+                  </div>
+                  <div className="col-contestant-profile-items">
+                    <p className="col-contestant-profile__key">Country:</p>
+                    <p className="col-contestant-profile__value">Nigeria</p>
+                  </div>
+                  <div className="col-contestant-profile-items">
+                    <p className="col-contestant-profile__key">State:</p>
+                    <p className="col-contestant-profile__value">Lagos</p>
+                  </div>
+                  {hasVideo ? (
+                    <div>
+                      <p className="col-contestant-profile__key">
+                        YouTube Video:
+                      </p>
+                      <iframe
+                        width="100%"
+                        height="200px"
+                        src={videourl}
+                        title="Contestant video 2"
+                        frameBorder="0"
+                        allow="autoplay; encrypted-media"
+                        allowFullScreen
+                      />
+                    </div>
+                  ) : (
+                    <div className="youtube-video-upload">
+                      <div className="youtube-video-container">
+                        <p>Please provide the link to your youtube video</p>
+                      </div>
+                      <input
+                        className="youtube-video-input"
+                        type="text"
+                        placeholder="Your youtube link here"
+                        onChange={({ target }) =>
+                          this.setState({ url: target.value })
+                        }
+                      />
+                      <span>
+                        <button
+                          className="youtube-video-save-button"
+                          onClick={this.onSave}
+                        >
+                          Save Video
+                        </button>
+                      </span>
+                    </div>
+                  )}
+                  <div className="col-contestant-button">
+                    <button className="col-contestant-update-button">
+                      Update Profile
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
           </div>
         </div>
 
