72450e15bb5853ac2f161d1f68bd9e60d114194f
jihdeh <jide.b.tade@gmail.com>
2018-05-31 23:57:05 +0100

    back to 1k

--- client/src/containers/register.js
+++ client/src/containers/register.js
@@ -114,7 +114,7 @@ class Register extends Component {
     var handler = window.PaystackPop.setup({
       key: process.env.REACT_APP_PAYSTACK_KEY,
       email: email,
-      amount: 10000, //in kobo
+      amount: 100000, //in kobo
       ref: "" + Math.floor(Math.random() * 1000000000 + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
       firstname: firstName,
       lastname: lastName,
